3db968aae4300b29f6bc33c5163bd4f0f7b1bc81
Maryna Bolotska <42913418+mabolotska@users.noreply.github.com>
2023-07-28 23:39:32 +0200

    detailview

---
+++ IconStore/IconDetailViewController.swift
@@ -0,0 +1,29 @@
+//
+//  IconDetailViewController.swift
+//  IconStore
+//
+//  Created by Maryna Bolotska on 28/07/23.
+//
+
+import UIKit
+
+class IconDetailViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
