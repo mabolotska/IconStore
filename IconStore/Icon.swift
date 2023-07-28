d6c1c49c0fa3b167e14fa3490af5475c85917e30
Maryna Bolotska <42913418+mabolotska@users.noreply.github.com>
2023-07-28 12:14:09 +0200

    Set Cell

---
+++ IconStore/Icon.swift
@@ -0,0 +1,8 @@
+//
+//  Icon.swift
+//  IconStore
+//
+//  Created by Maryna Bolotska on 28/07/23.
+//
+
+import Foundation
---
+++ IconStore/IconCollectionViewCell.swift
@@ -0,0 +1,12 @@
+//
+//  IconCollectionViewCell.swift
+//  IconStore
+//
+//  Created by Maryna Bolotska on 28/07/23.
+//
+
+import UIKit
+
+class IconCollectionViewCell: UICollectionViewCell {
+    
+}
